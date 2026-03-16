5e88ab1be45cde977a6c0c005d1712b8cfabe954
Bhargavi <nanduattili000@gmail.com>
2026-03-16 20:30:47 +0530

    Add function to generate mel spectrogram from audio

---
+++ spectrogram_generation.py
@@ -0,0 +1,13 @@
+import librosa
+import numpy as np
+
+def generate_mel_spectrogram(audio, sr=16000):
+    mel_spec = librosa.feature.melspectrogram(
+        y=audio,
+        sr=sr,
+        n_mels=128
+    )
+    
+    mel_db = librosa.power_to_db(mel_spec, ref=np.max)
+    
+    return mel_db
